3b65a169419ca90618235da795f2178fbfbfca65
Alberto Behar <albertobehar05@gmail.com>
2023-06-15 11:22:47 +0200

    prova bottone

---
+++ blog.js
@@ -0,0 +1,10 @@
+<html> 
+<section id="whenSignedOut"> 
+<button id="signInBtn"> Sign in with google</button>
+</section>
+
+<section id="whenSignedIn" hidden="true"> 
+<div id="userDetails"></div>
+<button id="signOutBtn"> Sign out</button>
+</section>
+</html>
