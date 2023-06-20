b62703e0cd169494427a4eb5bdfeaf394cf15d9b
Faiulo <faiulo>
2023-06-20 09:51:05 +0200

    Configurazione astro-firebase

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
+import myAdapter from '@astrojs/my-adapter';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: myAdapter(),
+});
