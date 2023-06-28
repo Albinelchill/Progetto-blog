df18ca75e1b3667d43eb6609bf91bbe08205160d
Alberto Behar <albertobehar05@gmail.com>
2023-06-28 16:06:03 +0200

    aggiunta vercel

---
+++ .vercel/output/static/_astro/hoisted.fac3da51.js
@@ -0,0 +1,1 @@
+import"https://cdn.tailwindcss.com";import"https://cdn.tailwindcss.com/2.2.19/tailwind.min.js";
--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -1,6 +1,6 @@
-import type { APIRoute } from "astro";
 
-export const get: APIRoute = async ({ redirect, cookies }) => {
+
+export const get = async ({ redirect, cookies }) => {
   cookies.delete("session", {
     path: "/",
   });
